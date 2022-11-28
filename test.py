51d58aa51bbd3adf6229a2c22c3a80f5042c5c00
cylqqqcyl <2540251142@qq.com>
2022-11-28 13:48:00 +0800

    GaussDB测试样例test.py

--- test.py
+++ test.py
@@ -1,40 +1,52 @@
-import psycopg2
-conn = psycopg2.connect(dbname="gauss_user08",
-                            user="DB_USER08",
-                            password="DB_USER08@123",
-                            host="192.168.0.198",
-                            port="3306")
-conn.set_client_encoding('utf8')
-cur = conn.cursor()
-cur.execute(
-'''
-CREATE TABLE COMPANY(
-    ID INT PRIMARY KEY     NOT NULL,
-    NAME           TEXT    NOT NULL,
-    AGE            INT     NOT NULL,
-    ADDRESS        CHAR(50),
-    SALARY         REAL);
-''')
-# 数据插入
-cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
-      VALUES (1, 'Paul', 32, 'California', 20000.00 )");
+import pymysql
 
-cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
-      VALUES (2, 'Allen', 25, 'Texas', 15000.00 )");
+# 打开数据库连接
+db = pymysql.connect(user="DB_USER08",
+                     password="DB_USER08@123",
+                     host="124.70.7.2",
+                     port=3306,
+                     database="user08db",
+                     charset='utf8')
+# 端口号3306，utf-8编码，否则中文有可能会出现乱码。
+# 使用 cursor() 方法创建一个游标对象 cursor
+cursor = db.cursor()
+# 如果存在表则删除
+cursor.execute("DROP TABLE IF EXISTS Employee")
 
-cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
-      VALUES (3, 'Teddy', 23, 'Norway', 20000.00 )");
-
-cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
-      VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 )");
-conn.commit()
-
-# 数据查询
-cur.execute("SELECT id, name, address, salary  from COMPANY")
-rows = cur.fetchall()
-for row in rows:
-   print("ID = ", row[0])
-   print("NAME = ", row[1])
-   print("ADDRESS = ", row[2])
-   print("SALARY = ", row[3])
-conn.close()
+# 使用 execute()  方法执行 SQL 查询
+sql = '''CREATE TABLE Employee (
+         Id  CHAR(20) NOT NULL,
+         Name  CHAR(20),
+         Age INT,  
+         SEX CHAR(1),
+         Salary FLOAT )'''
+try:
+    cursor.execute(sql)
+except Exception as e:
+    db.rollback()  # 如果出错就回滚并且抛出错误收集错误信息。
+    print("Error!:{0}".format(e))
+# finally:
+#     db.close()
+# # 关闭数据库连接
+# cursor = db.cursor()
+#SQL语句
+sql="SELECT * FROM Employee"
+try:
+    cursor.execute(sql)
+    results = cursor.fetchall() #获取全部结果集。  fetchone 查询第一条数据
+    if not results: #判断是否为空。
+        print("数据为空！")
+    else:
+        for row in results:
+            Id = row[0]
+            Name = row[1]
+            Age = row[2]
+            Sex = row[3]
+            Salary = row[4]
+            # 打印结果
+            print("id:{0}姓名:{1}年龄:{2}性别:{3}工资:{4}".format(Id,Name,Age,Sex,Salary))
+except Exception as e:
+    db.rollback()  #如果出错就会滚数据库并且输出错误信息。
+    print("Error:{0}".format(e))
+finally:
+    db.close()#关闭数据库。
