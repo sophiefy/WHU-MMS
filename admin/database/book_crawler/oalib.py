6eb5f48ae6a7948c10df2756501e19917cba39fa
Francis-Hu <2235306122@qq.com>
2022-12-07 21:01:11 +0800

    add user table generator

--- admin/database/book_crawler/oalib.py
+++ admin/database/book_crawler/oalib.py
@@ -3,7 +3,8 @@ from selenium.webdriver.common.by import By
 import time
 import csv
 from tqdm import tqdm
-
+import requests
+from lxml import etree
 '''
 url format:
 http://search.dangdang.com/?key=%C6%B7%B8%F1%D1%F8%B3%C9&act=input&sort_type=sort_score_desc&page_index='+str(page+1)+'#J_tab
@@ -15,38 +16,57 @@ driver = webdriver.Edge(options=opt)
 
 
 def get_basic_info(query, page_num):
+
+
+    # headers = {
+    #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.67"
+    # }
     url = 'http://search.dangdang.com/?key={}&act=input&sort_type=sort_score_desc&page_index={}'.format(query, page_num)
+    # try:
+    #     page_text = requests.get(url=url, headers=headers).text
+    # except Exception as e:
+    #     print(e)
+    #     return None
+    #
+    # etree_html = etree.HTML(page_text)
+    # elements = etree_html.xpath('//ul[@class="bigimg"]/li')
     driver.get(url)
-    elements = driver.find_elements(By.XPATH, '//ul[@class="bigimg"]/li')
+
     basic_info = []
-
-    for element in elements:
-        try:
-            info = element.find_element(By.XPATH, 'a')
-            name = info.get_attribute('title')
-            book_url = info.get_attribute('href')
-            info_list = element.find_element(By.CLASS_NAME, 'search_book_author').find_elements(By.XPATH, 'span')
-            author = info_list[0].text
-            release_date = info_list[1].text.replace('/', '')
-            press = info_list[2].text.replace('/', '')
-            ISBN = get_ISBN(book_url)
-        except Exception as e:
-            print(e)
-        else:
-            if len(ISBN) == 13:
-                basic_info.append([name, author, press, release_date, ISBN])
-                driver.back()
-                time.sleep(1)
-            else:
-                pass
-
-
+    elements = driver.find_elements(By.XPATH, '//ul[@class="bigimg"]/li')
+    for i, element in enumerate(elements):
+        info = element.find_element(By.XPATH, './a')
+        name = info.get_attribute('title')
+        book_url = info.get_attribute('href')
+        info_list = element.find_element(By.CLASS_NAME, 'search_book_author').find_elements(By.XPATH, './span')
+        author = info_list[0].text
+        release_date = info_list[1].text.replace('/', '')
+        press = info_list[2].text.replace('/', '')
+        ISBN = get_ISBN(book_url)
+        basic_info.append([name, author, press, release_date, ISBN])
+        driver.back()
+    # for element in elements:
+    #     name = element.xpath('./a/@title')[0]
+    #     book_url = element.xpath('./a/@href')[0]
+    #     book_url = 'http:' + book_url
+    #     print(book_url)
+    #     author = element.xpath('.//p[@class="search_book_author"]/span[1]/text()')[0]
+    #     release_date = element.xpath('.//p[@class="search_book_author"]/span[2]/text()')[0]
+    #     press = element.xpath('.//p[@class="search_book_author"]/span[3]/text()')[0]
+    #     release_date = release_date.replace('/', '')
+    #     press = press.replace('/', '')
+    #     ISBN = get_ISBN(book_url)
+    #     basic_info.append([name, author, press, release_date, ISBN])
 
     return basic_info
 
 
-def get_ISBN(book_url):
+def get_ISBN(book_url, fisrt=False):
     driver.get(book_url)
+    if fisrt:
+        time.sleep(10)
+    else:
+        time.sleep(1)
     ISBN = driver.find_element(By.XPATH, '//ul[@class="key clearfix"]/li[contains(text(),"ISBN")]').text
     ISBN = ISBN.replace('国际标准书号ISBN：', '')
 
@@ -63,8 +83,8 @@ if __name__ == '__main__':
     save_path = f'sample_data/dangdang.csv'
     total_page = 100
 
-    get_ISBN('http://product.dangdang.com/11307171768.html')
-    time.sleep(10)
+    sample_ISBN = get_ISBN('http://product.dangdang.com/11307171768.html', True)
+    print(sample_ISBN)
 
     with open(save_path, 'w', encoding='utf-8', newline='') as fp:
         writer = csv.writer(fp)
